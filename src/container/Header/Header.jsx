2ca6ebccf03b0af6b69378f577129e20cb840af6
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-11-04 18:15:23 +0800

    Header - fix styles

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -5,17 +5,18 @@ import { images } from '../../constants';
 import './Header.css';
 
 const Header = () => (
-  <div className="app__header app_wrapper section_padding" id="home">
-    <div className="app_wrapper_info">
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
       <SubHeading title="Chase the new flavour" />
       <h1 className="app__header-h1">The Key to Fine Dining</h1>
-      <p className="p_opensans" style={{ margin: '2rem 0'}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa iure pariatur doloribus molestiae est. Corrupti enim excepturi aperiam dignissimos beatae.
+      <p className="p__opensans" style={{ margin: '2rem 0'}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa iure pariatur doloribus molestiae est. Corrupti enim excepturi aperiam dignissimos beatae.
       </p>
       <button type="button" className="custom__button">Explore Menu</button>
     </div>
-    <div className="app_wrapper_img">
+    <div className="app__wrapper_img">
       <img src={images.welcome} alt="welcome" />
     </div>
+    test
   </div>
 );
 
