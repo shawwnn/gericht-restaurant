759c1d6210e8a581fc209788e7ef4738c7728092
Shawn Lapuz <shangel.lapuz@gmail.com>
2022-11-04 17:31:02 +0800

    Header Banner

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 import './Header.css';
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app_wrapper section_padding" id="home">
+    <div className="app_wrapper_info">
+      <SubHeading title="Chase the new flavour" />
+      <h1 className="app__header-h1">The Key to Fine Dining</h1>
+      <p className="p_opensans" style={{ margin: '2rem 0'}}>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ipsa iure pariatur doloribus molestiae est. Corrupti enim excepturi aperiam dignissimos beatae.
+      </p>
+      <button type="button" className="custom__button">Explore Menu</button>
+    </div>
+    <div className="app_wrapper_img">
+      <img src={images.welcome} alt="welcome" />
+    </div>
   </div>
 );
 
